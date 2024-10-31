91df12bd7c52b24c1f514564b1f1a5835c58853f
Ritik Mahakalkar <153834897+Ritik-Mahakalkar@users.noreply.github.com>
2024-10-31 22:20:44 +0530

    main

---
+++ src/Components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div>
+        <h1>hello,<span>Hello World</span></h1>
+      
+    </div>
+  )
+}
+
+export default HelloWorld
